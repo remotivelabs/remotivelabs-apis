209d754df7bf207e9e30abc91cdc682db1a3eb3b
Johan Rask <johan.rask@remotivelabs.com>
2023-10-26 07:03:01 +0200

    fix: Generate python stubs with docker and make sure to use protobuf 3.19.2

---
+++ python/remotivelabs-broker/build_proto.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+OUT=remotivelabs/broker/generated/sync/
+
+# protoc -I ../../proto ../../proto/*.proto --python_out=$OUT --grpc_python_out=$OUT
+# python3 -m grpc_tools.protoc --python_out=helloworld --grpc_python_out=helloworld --pyi_out=helloworld -I ../protos ../protos/helloworld.proto
+
+# TODO - Get from dockerhub once we know that this works properly on all platforms
+docker build -t remotivelabs/grpcio-tools-protobuf-3.19.2 .
+
+docker run  \
+  -v $(pwd)/../../protos:/protofile \
+  -v $(pwd)/$OUT:/output \
+  -e "protofile=*.proto" \
+  remotivelabs/grpcio-tools-protobuf-3.19.2
+
+#python3  -m grpc_tools.protoc -I ../../protos ../../protos/*.proto --python_out=$OUT --grpc_python_out=$OUT
+python3 misc/fix_import_statements.py
+
+echo "Successfully generated python stubs"
--- python/remotivelabs-broker/misc/build_proto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-OUT=remotivelabs/broker/generated/sync/
-
-# protoc -I ../../proto ../../proto/*.proto --python_out=$OUT --grpc_python_out=$OUT
-# python3 -m grpc_tools.protoc --python_out=helloworld --grpc_python_out=helloworld --pyi_out=helloworld -I ../protos ../protos/helloworld.proto
-
-python3 -m grpc_tools.protoc -I ../../protos ../../protos/*.proto --python_out=$OUT --grpc_python_out=$OUT
-python3 misc/fix_import_statements.py
