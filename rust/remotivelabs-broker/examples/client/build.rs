1f2cccbdaa604295fe66c5faa7396adabb0c359c
Martin Sandgren <martin.sandgren@remotivelabs.com>
2024-04-09 15:30:46 +0200

    refactor: Use default protobuf in Rust
    Instead of using copy of protobuf files in local Rust project. Generate
    at compile time.

--- rust/remotivelabs-broker/examples/client/build.rs
+++ rust/remotivelabs-broker/examples/client/build.rs
@@ -4,14 +4,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .out_dir("src/beamy_api")
         .compile(
             &[
-                "protos/common.proto",
-                "protos/diagnostics_api.proto",
-                "protos/functional_api.proto",
-                "protos/network_api.proto",
-                "protos/system_api.proto",
-                "protos/traffic_api.proto",
+                "common.proto",
+                "diagnostics_api.proto",
+                "functional_api.proto",
+                "network_api.proto",
+                "system_api.proto",
+                "traffic_api.proto",
             ],
-            &["protos"],
+            &["../../../protos"],
         )?;
     Ok(())
 }
