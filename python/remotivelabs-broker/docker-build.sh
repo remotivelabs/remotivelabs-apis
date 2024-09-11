9783b46cdf69819eda0862bbcfb116e1a316b6de
Tomas Nilsson <tomas.nilsson@remotivelabs.com>
2024-09-11 07:49:15 +0000

    fix(grpc-asyncio): fix review comments

--- python/remotivelabs-broker/docker-build.sh
+++ python/remotivelabs-broker/docker-build.sh
@@ -4,18 +4,18 @@
 #
 set -e
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "$0")" && pwd)
 TAG=remotivelabs/python-api-build-image
 
-docker build -t ${TAG} -f ${SCRIPT_DIR}/docker/Dockerfile ${SCRIPT_DIR}
+docker build -t "${TAG}" -f "${SCRIPT_DIR}/docker/Dockerfile" "${SCRIPT_DIR}"
 
 # set args to allow build to run in non-tty shells
 ARGS="-it"
 [ "${NO_TTY}" == "true" ] && ARGS="-i"
 
 docker run \
-    -u $(id -u):$(id -g) \
-    -v ${SCRIPT_DIR}/../../:/app  \
+    -u "$(id -u):$(id -g)" \
+    -v "${SCRIPT_DIR}/../../:/app"  \
     -e "protofile=*.proto"  \
     -w /app  \
-    ${ARGS} ${TAG}
+    "${ARGS}" "${TAG}"
