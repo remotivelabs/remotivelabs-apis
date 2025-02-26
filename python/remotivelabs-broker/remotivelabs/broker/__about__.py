62f813a93085e6a142c396c6218e68af877670a9
Johan Karlberg <johan.karlberg@remotivelabs.com>
2025-02-26 21:57:52 +0100

    chore: remotivelabs.broker changes repository
    - proto api is still published in this repositroy
    - Package is still published to pypi, but source changes repo
    - This repo no longer builds and documentation, that is also moved

--- python/remotivelabs-broker/remotivelabs/broker/__about__.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2022-present remotiveLabs <support@remotivelabs.com>
-#
-# SPDX-License-Identifier: Apache-2.0
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version("remotivelabs-broker")
-except PackageNotFoundError:
-    __version__ = "unknown"
