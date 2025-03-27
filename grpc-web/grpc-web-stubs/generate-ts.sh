c6576980d8266ac2bb753886ce855b968a7a04b6
Albin Theander <albin.theander@remotivelabs.com>
2025-03-27 15:19:40 +0100

    Handle proto files in subfolders

--- grpc-web/grpc-web-stubs/generate-ts.sh
+++ grpc-web/grpc-web-stubs/generate-ts.sh
@@ -16,15 +16,20 @@ docker run  \
   -e "grpc_web_import_style=commonjs+dts" \
   -e "protofile=*.proto" remotivelabs/grpc-web-generator
 
-# Disable eslint on each js file
-(cd $TMP && \
-for filename in *.js; do
-  echo "Adding /* eslint-disable */ to $filename"
-  printf "/* eslint-disable */\n" | cat - $filename > "../$OUTPUT/$filename"
-done )
 
-cp $TMP/*.ts $OUTPUT
-rm -rf $TMP
+# Copy files and disable eslint on each js file
+(
+find $TMP -type f | while read -r filepath; do
+  relative_path="${filepath#$TMP/}"
+  echo "Processing $relative_path"
+  output_path="$OUTPUT/$relative_path"
+  mkdir -p "$(dirname "$output_path")"
+  if [[ $filepath == *.js ]]; then
+    printf "/* eslint-disable */\n" | cat - "$filepath" > "$output_path"
+  else
+    cp "$filepath" "$output_path"
+  fi
+done )
 
 npx tsc
 
